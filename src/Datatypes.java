9edec01c9e5486de6d6f7bbb00f46278bf2e4ab9
Aysel63 <aysel.abdulzade05@gmail.com>
2024-06-30 01:40:55 +0400

    FIRST MESSAGE

---
+++ src/Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        //Table
+        int legs=4;
+        double area;
+        String color;
+        boolean sold;
+        String shape;
+        int heightInMM;
+        int heightInCM;
+        //Pascal case  - class name
+        // camel case- variable name,method names
+        //snake_case
+        //kebab-case
+    }
+}
