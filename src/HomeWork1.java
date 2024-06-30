3613bff2acf5acd38499093170ef99d83f237932
Aysel63 <aysel.abdulzade05@gmail.com>
2024-07-01 02:16:22 +0400

    homework

---
+++ src/HomeWork1.java
@@ -0,0 +1,40 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        //Car
+        String brand ="BMW";
+        long price =30000L;
+        String color ="White";
+
+        //Book
+        String genre ="detective";
+        int page_count = 350;
+        String author ="Agatha Christie";
+        boolean interesting  =true;
+
+        //Student
+        String name = "Aysel";
+        int age =21;
+        boolean high_education = true;
+        double average = 88.4;
+
+
+        // Table
+        String shape= "square";
+        double heighInCM = 50.5;
+        int legs =4;
+        String color ="black";
+        boolean sold = false;
+
+        //TV
+        String brand ="Samsung";
+        double diagonalInCM=124.5;
+        String type ="LED";
+
+
+
+
+
+
+
+    }
+}
